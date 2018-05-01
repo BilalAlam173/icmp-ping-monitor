2398504c2872930018064a7e90db73979e9180de
Bilal Alam <bilalalam173@gmail.com>
2018-05-02 01:05:33 +0500

    db apis for CRUD

--- controller.js
+++ controller.js
@@ -5,41 +5,56 @@ const User=require('./model');
 module.exports = {
 
  insert : (req,res)=>{
+     console.log(User);
 
-    const User = new Car({
+    const user = new User({
          name:req.body.name,
-         color:req.body.ip
+         ip:req.body.ip,
+         startTime:req.body.startTime,
      });
 
      // call save funtion on that model's instance
-     car.save((err)=>{
+     user.save((err)=>{
+        console.log('test')
          if(err){
              // return error
              res.status(500).json({message :"something went wrong"});
          }else{
              // return success
+             console.log('test')
              res.status(200).json({message:"user added successfully"});
          }
          
      });
  },
  get : (req,res)=>{
+    User.find({}, function(err, docs) {
+        if (!err){
+            res.send(docs);
+        } else {
+            res.sendStatus(500);
+        }
+    });
+ },
+ update : (req,res)=>{
+    User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+        if (err) {
+            return res
+                .status(500)
+                .send({error: "unsuccessful"})
+        };
+        res.send({success: "success"});
+    });
+ },
+ delete : (req,res)=>{
 
-    const User = new Car({
-         name:req.body.name,
-         color:req.body.ip
-     });
-
-     // call save funtion on that model's instance
-     car.save((err)=>{
-         if(err){
-             // return error
-             res.status(500).json({message :"something went wrong"});
-         }else{
-             // return success
-             res.status(200).json({message:"user added successfully"});
-         }
-         
-     });
+    User.remove({ _id: req.params.id }, function(err) {
+        if (!err) {
+                res.sendStatus(200);
+        }
+        else {
+                res.sendStatus(500);
+        }
+    });
  }
 }
