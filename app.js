46655c75cc89e5ddee034879548729114613d7ad
Bilal Alam <bilalalam173@gmail.com>
2018-05-06 15:29:27 +0500

    Revert "cors"
    This reverts commit 84bfebc836d7eafbaf6eba2033cba37c78b185a9.

--- app.js
+++ app.js
@@ -57,11 +57,15 @@ function notifyChange(user) {
   })
 
 }
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://ping.elevate.tech");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.options('/', function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
+  res.header("Access-Control-Allow-Origin", "http://ping.elevate.tech");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 app.post('/user', ctrl.insert);
 app.get('/user', ctrl.get);
