e281b5b57c0a3275392cdf31b7927f89104b6615
Muhammad Bilal Alam <bilalalam173@gmail.com>
2018-06-13 06:45:11 +0500

    fix

--- frontend/src/app/global.ts
+++ frontend/src/app/global.ts
@@ -2,6 +2,6 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class global {
-  //url='http://ping.elevate.tech/api/'
-  url='http://localhost:3000/api/'
+  url='http://ping.elevate.tech/api/'
+  // url='http://localhost:3000/api/'
 }
