6b577c48990442364c828b7c552143b0ec814ee6
Muhammad Bilal Alam <bilalalam173@gmail.com>
2018-06-16 15:34:43 +0500

    http url changed

--- frontend/src/app/global.ts
+++ frontend/src/app/global.ts
@@ -3,5 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class global {
   // url='http://ping.elevate.tech/api/'
-  url='http://localhost:3000/api/'
+  // url='http://localhost:3000/api/';
+  url='http://127.0.0.1:3000/api/';
 }
